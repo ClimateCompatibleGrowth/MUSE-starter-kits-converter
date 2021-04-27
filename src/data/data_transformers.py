f196a091ffc88a42be875fa3009ab0341eb7beb8
Alexander Kell <alexander@kell.es>
2021-04-27 14:44:59 +0100

    imported all data

--- src/data/data_transformers.py
+++ src/data/data_transformers.py
@@ -10,13 +10,24 @@ class Transformer:
         self.input_path = Path(input_path)
         self.output_path = Path(output_path)
         self.folder = str(self.input_path).split("/")[-1]
+        self.raw_tables = self.get_data()
 
     def create_muse_dataset(self):
         logger = logging.getLogger(__name__)
-        logger.info("Getting full dataset.")
-        logger.info("Project dir: {}".format(PROJECT_DIR))
+        logger.info("Importing raw data for {}.".format(self.folder))
 
-    def installed_power_plants(self):
-        # pd.read_csv("")
+    def get_raw_data(self):
+        table_directories = glob.glob(str(self.input_path / Path("*.csv")))
+
+        tables = {}
+        for table_directory in table_directories:
+            table_name = table_directory.split("/")[-1].split("_")[0]
+            tables[table_name] = pd.read_csv(table_directory)
+
+        return tables
+
+    def convert_installed_power_plants(self):
+        plant_data = self.raw_tables['Table1']
+        
         pass
 
