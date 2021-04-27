17183f690668479ae998aa1fa8453edd6784625e
Alexander Kell <alexander@kell.es>
2021-04-27 11:22:18 +0100

    retrieved starter kit directories

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,21 +3,28 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+import glob
+import os
 
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
+@click.argument("input_filepath", type=click.Path(exists=True))
+@click.argument("output_filepath", type=click.Path())
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    logger.info("Converting starter kit data into a MUSE compatible form.")
+    logger.info("Converting the following folders:")
+    directories = glob.glob(str(project_dir) + "/" + str(input_filepath) + "/*/")
+    logger.info("directory_contents: {}".format(directories))
+    for item in directories:
+        logger.info(item)
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
